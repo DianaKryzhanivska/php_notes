00f4f8893334827b1c9c8a434221efcffeea111d
Diana Kryzhanivska <dianakryzhanivska@Noutbuk-Diana.local>
2024-08-07 17:45:55 +0300

    math

--- index.php
+++ index.php
@@ -38,11 +38,34 @@
         $num = -0.55; // float
         $str = "variable"; // string
         $bool = true; // boolean
-        echo $str . ': ' . $number . $num; // 50
+        echo $str . ': ' . $number . $num . '<br>'; // 50
 
     ?>
 
+    <?php
 
+    $x = 10;
+    $y = 20;
+
+    $x += 10; // $x = $x + 10
+    echo $x . '<br>'; // 20
+
+    $x++;
+    $y--;
+
+    echo $x . '<br>'; // 21
+    echo $y . '<br>'; // 19
+
+    echo M_PI . '<br>'; // 3.14
+
+    echo abs(-22) . '<br>'; // 22 (повертає позитивне число)
+    echo ceil(3.1) . '<br>'; // 4 (округлення до більшого)
+    echo floor(3.9) . '<br>'; // 3 (округлення до меншого)
+    echo round(3.4134314312, 2) . '<br>'; // 3.41 
+    echo mt_rand(1, 20) . '<br>'; // рандом число від 1 до 20
+    echo min(9, 6, 3) . '<br>'; // 3
+    echo max(9, 6, 3) . '<br>'; // 9
+    ?>
     
 </body>
 </html>
