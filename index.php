62bba347c3f0e254c17c86c64eee1c6dec51bac6
Diana Kryzhanivska <dianakryzhanivska@Noutbuk-Diana.local>
2024-08-07 17:07:03 +0300

    base rules

--- index.php
+++ index.php
@@ -10,8 +10,38 @@
     <h1>Hello</h1>
 
     <?php
-        echo "this is php script"
+        echo "this is <b>php</b> script";
+        echo "<br>hello";
+        echo "\"";
+        echo '<br>"<br>';
+
+        // Comment
+        # Comment
+
+        /*
+            comment
+        */
     ?>
+
+    <?php
+        // створити константу - прийнято записувати у ВЕРХНЬОМУ РЕГІСТРІ
+        define(MY_AGE, 18);
+        echo MY_AGE . '<br>'; // без $
+
+        // не строга типізація
+        $a = 0.5;
+        $b = '0.5';
+        echo $a + floatval($b); // 1 (не строга типізація) 
+        // intval 
+
+        $number = 5; // int (integer)
+        $num = -0.55; // float
+        $str = "variable"; // string
+        $bool = true; // boolean
+        echo $str . ': ' . $number . $num; // 50
+
+    ?>
+
 
     
 </body>
